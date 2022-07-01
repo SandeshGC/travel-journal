830966107bf9506e8a9463efc11f833f7cc7ebf7
Sandesh GC <gcsandesh01@gmail.com>
2022-07-01 09:30:08 +0545

    fixed image bugs and added places

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,15 @@
 import Data from "../data.js";
 import Card from "../components/Card";
 export default function Content() {
-  let arrayOfCards = Data.map((eachData) => <><Card item={eachData} /><hr className="end-of-card"/></>);
+  let arrayOfCards = Data.map((eachData) => (
+    <>
+      <Card item={eachData} />
+      <hr className="end-of-card" />
+    </>
+  ));
   return (
     <section>
+      {/* <div className="cards-container">{arrayOfCards}</div> */}
       {arrayOfCards}
     </section>
   );
--- src/data.js
+++ src/data.js
@@ -1,17 +1,27 @@
 export default [
-  {
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    {
+      title: "Maya Devi Temple",
+      location: "Lumbini, Nepal",
+      googleMapsUrl: "https://goo.gl/maps/bn8QNHTaQrU5rH8C9",
+      startDate: "28 June, 2022",
+      endDate: "30 June, 2022",
+      description:
+      "Maya Devi Temple is an ancient Buddhist temple situated at the UNESCO World Heritage Site of Lumbini, Nepal and very close to the Indo–Nepal border. It is the main temple at Lumbini, a site traditionally considered the birthplace of Gautama Buddha.",
+      imageUrl: "https://source.unsplash.com/aNU8MnzWhKo",
+    },
+    {
+    title: "Bhaktapur Durbar Square",
+    location: "Kathmandu, Nepal",
+    googleMapsUrl: "https://goo.gl/maps/hfmcEwyfxfeq2VHe7",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    "Bhaktapur Durbar Square is a former royal palace complex located in Bhaktapur, Nepal. It housed the Malla kings of Nepal from 14th to 15th century and the kings of the Kingdom of Bhaktapur from 15th to late 18th century until the kingdom was conquered in 1769.",
+    imageUrl: "https://source.unsplash.com/JUvPVTuZONY",
   },
   {
     title: "Fewa Lake",
-    location: "Nepal",
+    location: "Pokhara, Nepal",
     googleMapsUrl: "https://goo.gl/maps/fL7gpqRmYPKxHNrb9",
     startDate: "28 June, 2019",
     endDate: "30 June, 2019",
@@ -20,18 +30,18 @@ export default [
     imageUrl: "https://source.unsplash.com/QQ9j5GEUkxU",
   },
   {
-    title: "Fewa Lake",
-    location: "Nepal",
+    title: "Pashupatinath Temple",
+    location: "Kathmandu, Nepal",
     googleMapsUrl: "https://goo.gl/maps/NtHATNs61nnWxmx58",
     startDate: "28 June, 2022",
     endDate: "30 June, 2022",
     description:
-      "Phewa Lake, Phewa Tal or Fewa Lake is a freshwater lake in Nepal formerly called Baidam Tal located in the south of the Pokhara Valley that includes Pokhara city; parts of Sarangkot and Kaskikot",
+    "Pashupatinath Temple is a Hindu temple dedicated to Pashupati, and is located in Kathmandu, Nepal. This temple was classified as a World Heritage Site in 1979.",
     imageUrl: "https://source.unsplash.com/R2sI4cwPXqQ",
   },
   {
     title: "Swayambhunath Stupa",
-    location: "Nepal",
+    location: "Kathmandu, Nepal",
     googleMapsUrl: "https://goo.gl/maps/9p9r1uWdd79Khnb27",
     startDate: "28 June, 2022",
     endDate: "30 June, 2022",
@@ -39,4 +49,5 @@ export default [
       "Swayambhu is an ancient religious complex atop a hill in the Kathmandu Valley, west of Kathmandu city. The Tibetan name for the site means 'Sublime Trees', for the many varieties of trees found on the hill. However, Shing.kun may be of the local Nepal Bhasa name for the complex, Swayambhu, meaning 'self-sprung'.",
     imageUrl: "https://source.unsplash.com/uA3cYm0mN_s",
   },
+  
 ];
